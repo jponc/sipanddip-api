28928aa2ec277aee23e989fc7ea9937d862154c8
Julian Alfonso Ponce <hello@julianponce.com>
2017-09-08 14:13:54 +0800

    Updated trigger points and added more inventory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,39 @@
 inventory_hash = {
   hot_chix: 2,
-  chicken_strips: 3,
+  chicken_strips: 5,
   fish_fillet: 3,
-  squid_rings: 3,
+  squid_rings: 2,
 
-  french_fries: 4,
-  onion_rings: 2,
+  french_fries: 8,
+  onion_rings: 1,
   potato_wedges: 4,
-  mojos: 3,
+  mojos: 2,
 
-  house_blend: 8,
-  red_tea: 8,
-  cucumber: 8,
-  lemonade: 8,
-
+  house_blend: 5,
+  red_tea: 5,
+  cucumber: 5,
+  lemonade: 5,
 
   water: 3,
-  big_cups: 1000,
-  separator: 1000,
-  plastic_gloves: 5,
+  big_cups: 1_200,
+  separator: 1_200,
+  plastic_gloves: 10,
   stick: 5,
-  trash_bag: 5,
+  trash_bag: 50,
 
   signature_dip: 1,
   garlic_mayo: 1,
   honey_mustard: 1,
 
-  oil: 2,
+  oil: 1,
   corn_starch: 1,
-  flour: 1
+  flour: 1,
+
+  # Addiotional
+  cup_lids: 100,
+  straw: 1,
+  dip_container: 200,
+  tissue: 2
 }
 
 inventory_hash.each do |inventory_slug, trigger_count|
