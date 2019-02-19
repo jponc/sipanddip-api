3da7a95985a40a81ab436b9fc7fe5894c308b0f4
Julian Alfonso Ponce <julian@glamcorner.com.au>
2019-02-20 01:02:03 +1100

    Updated crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,10 @@
 set :output, "/opt/app/log/cron.log"
 
 # 11:30 PM PHT
-# every 1.day, at: '3:30pm' do
-#   rake "daily_generate:daily_record"
-# end
-#
+every 1.day, at: '3:30pm' do
+  command "bundle exec rake daily_generate:daily_record"
+end
+
 every 5.minutes do
-  rake "daily_generate:daily_record"
+  command "echo #{Time.now}"
 end
