301592e0130c0aa49a05c8de7c27cdb354dcba5b
Julian Alfonso Ponce <julian@glamcorner.com.au>
2019-03-11 20:21:28 +1100

    Added every 5 minute test

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,14 @@
 set :output, "/opt/app/log/cron.log"
 
 # 11:30 PM PHT
-every 1.day, at: '3:30pm' do
+# every 1.day, at: '3:30pm' do
+#   command "bundle exec rake daily_generate:daily_record"
+# end
+#
+every 5.minutes do
   command "bundle exec rake daily_generate:daily_record"
 end
 
 every 5.minutes do
-  command "echo #{Time.now}"
+  command "echo `date`"
 end
